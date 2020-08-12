384cc74a6dfc3a4cb752ce0115d022a778baa65e
royFloresNyc <royfloresnyc@gmail.com>
2020-08-12 13:07:56 -0400

    Partial insert.sql and querying.rb

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Series1", 1, 1), (2, "Series2", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "Subgenre1"), (2, "Subgenre2");
+
+INSERT INTO authors (id, name) VALUES (1, "Roy Flores"), (2, "David Kirsch");
+
+INSERT INTO books (id, title, year, series_id) VALUES (1,"Frolicking in Montenegro", 2019, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (2,"Living in Tulum", 2018, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (3,"Travel on a Budget", 2);
+
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
+
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
+INSERT INTO characters () VALUES ();
